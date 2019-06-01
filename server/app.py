210155a6786f0da4b2b9671fa3f0d92981e2b12a
Truong Hoang <truong@codelynx.io>
2019-06-01 23:45:30 +0700

    break changing: create 2 folder for client and server

---
+++ server/app.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
