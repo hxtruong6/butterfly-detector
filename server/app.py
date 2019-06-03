b53389ea39d8925ac8d63a5385e3b962db0c7320
Truong Hoang <truong@codelynx.io>
2019-06-03 13:29:22 +0700

    feat: create post get image end point

--- server/app.py
+++ server/app.py
@@ -2,15 +2,37 @@ import os
 from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = '/path/to/the/uploads'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+UPLOAD_FOLDER = './'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/image', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        print("xxx001 " + request.files)
+        if 'image' not in request.files:
+            flash('No file part')
+            return "No image"
+        file = request.files['image']
+        print("xxx 001 " + file)
+        if file.filename == '':
+            flash('No selected file')
+            return "No image"
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return "The processed image"
 
 
 if __name__ == '__main__':
